af8cc4f7173f8f7512ec5e915910277906078383
Aaron Grainer <aaronlimfz@gmail.com>
2021-05-28 23:15:52 +0800

    Updated GPT2 trainer and inferencer

--- ml/inferencer.py
+++ ml/inferencer.py
@@ -1,4 +1,4 @@
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, AutoModelForCausalLM
 
 import torch
 
@@ -7,11 +7,11 @@ from ml.config import global_config as gc
 import os
 
 
-class IntentInferencer:
+class GPT2Inferencer:
     def __init__(self):
         self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
-        self.model_path = os.path.join(gc.gpt2_checkpoint_path, gc.gpt2_model_torchscript_filename)
+        self.model_path = os.path.join(gc.gpt2_checkpoint_path, gc.gpt2_model_path)
 
         self.model_checkpoint = 'gpt2'
         self.max_length = 100
@@ -20,27 +20,33 @@ class IntentInferencer:
 
     def _initialize(self):
         if os.path.exists(self.model_path):
-            print('GPT2 torchscript checkpoint found, initializing model...')
+            print('GPT2 checkpoint found, initializing model...')
             self.tokenizer = AutoTokenizer.from_pretrained(self.model_checkpoint)
-            self.gpt2_model = torch.jit.load(self.model_path)
-            self.gpt2_model.eval()
+            self.gpt2_model = AutoModelForCausalLM.from_pretrained(self.model_path)
         else:
-            print("GPT2 torchscript checkpoint not found, please train the model.")
+            print("GPT2 checkpoint not found, please train the model.")
             self.model = None
 
     def predict(self):
         if not self.gpt2_model: 
             raise ModelNotTrained
 
-        output = self.gpt2_model(output.to(self.device))
-        output = self.softmax(output)
-        output_idx = torch.argmax(output)
-        output = output.flatten().tolist()
+        sequence = 'He began his premiership by forming a five-man war cabinet which included Chamerlain as Lord President of the Council, Labour leader Clement Attlee as Lord Privy Seal (later as Deputy Prime Minister), Halifax as Foreign Secretary and Labour\'s Arthur Greenwood as a minister without portfolio. In practice,'
+        inputs = self.tokenizer.encode(sequence, return_tensors='pt')
 
-        return output
+        outputs = self.gpt2_model.generate(inputs, max_length=200, do_sample=True)
+        text = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+        return text
 
 
 class ModelNotTrained(Exception):
     pass
+
+
+if __name__ == '__main__':
+    gpt2_inferencer = GPT2Inferencer()
+    text = gpt2_inferencer.predict()
+    print('text: ', text)
 
 
